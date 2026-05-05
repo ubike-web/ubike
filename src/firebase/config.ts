fceb9440433547dfd8d6f09bbdbf0540596e64b0
Raphael Mwandiki <raphaelmwandiki@gmail.com>
2026-05-05 14:16:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3531566474-d7ea1",
+  "appId": "1:1001188244228:web:f014ee79901c9fe9412e51",
+  "apiKey": "AIzaSyCaqEfpmARHbs34giHBNHb6XH3_BGwx45o",
+  "authDomain": "studio-3531566474-d7ea1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1001188244228"
+};
